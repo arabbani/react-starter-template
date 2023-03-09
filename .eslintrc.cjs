6e5161cd39a6220783b67a00614c876557377a19
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-10 01:03:37 +0530

    chore: :wrench: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        project: true,
+    },
+    settings: {
+        react: {
+          version: 'detect',
+        },
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": 0,
+    }
+}
