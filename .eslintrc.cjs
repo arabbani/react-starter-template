462810c9173e3b9e02f21b09dced132f6188895b
Arif Rabbani <arif.93rabbani@gmail.com>
2023-03-10 01:28:34 +0530

    chore: :wrench: update eslint, prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
